e391d444c1b79e273d5e98e9c2a60b24147f98c3
EPY <erick.pariona@hotmail.com>
2024-06-02 21:10:35 -0500

    Semana02

---
+++ POOI_Semana02/EmpleadoCodigoZeroException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOI_Semana02
+{
+    public class EmpleadoCodigoZeroException : Exception
+    {
+
+
+        public EmpleadoCodigoZeroException()
+        {
+        }
+
+        public EmpleadoCodigoZeroException(string message) : base(message)
+        {
+        }
+
+        public EmpleadoCodigoZeroException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- POOI_Semana02/Program.cs
+++ POOI_Semana02/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,82 @@ namespace POOI_Semana02
         static void Main(string[] args)
         {
 
+
+            
             List<Empleado> lista = new List<Empleado>();
-            lista.Add(new Empleado() { codigo = 1, nombre = "Erick", edad  = 30});
+            lista.Add(new Empleado() { codigo = 1, nombre = "p", edad = 18 });
+            try
+            {
+                bool existe = lista.Exists(x => x.codigo == 5);
+                if (existe)
+                {
+                    Console.WriteLine("Resultado : " + existe);
+                }
+                else
+                {
+                    throw new EmpleadoCodigoZeroException("Ocurrio un error al buscar");
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine("Error Mensaje : " + ex.Message);
+                Console.WriteLine("Nombre Error  : " + ex.GetType().Name);
+                Console.WriteLine("Ubicacion Metodo : " + ex.TargetSite);
+            }
+
+
+            /*
+            try
+            {
+                //int numerador = 12;
+                //int denominador = 0;
+                //int resultado;
+                //Exception Division Entre Cero
+                //resultado = numerador / denominador;
+                //Console.WriteLine("Resultado : " + resultado);
+                //Exception Formateo
+                //resultado = Convert.ToInt32("hola");
+
+                // Exception Fuera de Rango
+                //List<Empleado> lista = new List<Empleado>();
+                //lista.Add(new Empleado() { codigo = 1, nombre = "Erick", edad = 30 });
+                //Console.WriteLine(lista[1].nombre );
+                // Saldra error porque la lista solo tiene un objeto agregado
+                // que corresponde al indice 0
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.Message);
+                Console.WriteLine("Metodo de Error : " + ex.TargetSite);
+                Console.WriteLine("Tipo de Error : " + ex.GetType().Name);
+            }
+            finally
+            {
+                Console.WriteLine("Este bloque se ejecutara");
+            }
+            Console.WriteLine("Este linea se mostrara");
+
+
+            
+            */
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+        public static void probarListadoEmpleado()
+        {
+
+            List<Empleado> lista = new List<Empleado>();
+            lista.Add(new Empleado() { codigo = 1, nombre = "Erick", edad = 30 });
             lista.Add(new Empleado() { codigo = 2, nombre = "Pedro", edad = 50 });
             lista.Add(new Empleado() { codigo = 3, nombre = "miguel", edad = 10 });
             lista.Add(new Empleado() { codigo = 4, nombre = "david", edad = 15 });
@@ -22,6 +97,9 @@ namespace POOI_Semana02
             lista.Add(new Empleado() { codigo = 7, nombre = "sait", edad = 89 });
             lista.Add(new Empleado() { codigo = 8, nombre = "Hanss", edad = 8 });
             lista.Add(new Empleado() { codigo = 9, nombre = "Alex", edad = 5 });
+            lista.Add(new Empleado() { codigo = 10, nombre = "Alexa", edad = 33 });
+            lista.Add(new Empleado() { codigo = 11, nombre = "Juan", edad = 38 });
+
             mostrarArregloLista(lista);
             Console.WriteLine("***************************");
             // Ordenar de forma ascendente (Por la edad)
@@ -33,14 +111,32 @@ namespace POOI_Semana02
             mostrarArregloLista(listaOrdenDesc);
             Console.WriteLine("Tamano : " + listaOrdenDesc.Count);
             Console.WriteLine("************************************");
+            // Eliminar el codigo 5
 
+            lista.RemoveAll(x => x.codigo == 5);
+            mostrarArregloLista(lista);
+            Console.WriteLine("************************************");
 
+            Empleado objEmpleado = lista.Where(x => x.codigo == 1).First();
 
+            Console.WriteLine(
+                        "Empleado Codigo : " + objEmpleado.codigo + " - " +
+                        "Empleado Nombre : " + objEmpleado.nombre
+                            );
 
+            Console.WriteLine("****************ee********************");
+            // Seleccionar las personas que tengan edad mayor a 30
+            List<Empleado> listaPersonas =
+                lista.Where(x => x.edad > 30).ToList();
+            mostrarArregloLista(listaPersonas);
+            // Seleccionar las personas que tengan edad mayor a 40 y menor a 50
+            Console.WriteLine("************************************");
 
+            List<Empleado> listaPersona1 =
+                lista.Where(x => x.edad > 30 && x.edad < 50).ToList();
+            mostrarArregloLista(listaPersona1);
+            Console.WriteLine("************************************");
 
-            //probarListado();
-            Console.ReadKey();
         }
 
         public static void mostrarArregloLista(List<Empleado> lista)
@@ -53,8 +149,6 @@ namespace POOI_Semana02
             }
         }
 
-
-
         public static void mostrarArreglo(List<int> listado)
         {
             foreach (var i in listado)
@@ -62,7 +156,6 @@ namespace POOI_Semana02
                 Console.WriteLine(i);
             }
         }
-
 
         public static void probarListado()
         {
